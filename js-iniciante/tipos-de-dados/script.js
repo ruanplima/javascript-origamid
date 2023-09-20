3ce10cb2257f9a24b4be99bb08b69616feb5bb38
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2023-09-20 15:52:17 -0300

    Conhecimentos básicos em JS
    Conhecendo Variáveis e tipos de Dados

---
+++ js-iniciante/tipos-de-dados/script.js
@@ -0,0 +1,25 @@
+// Declare uma variável contendo uma string
+var nome = 'Paulinho Paula '
+console.log(nome + typeof nome)
+
+// Declare uma variável contendo um número dentro de uma string
+var theNumber = '10 '
+console.log(theNumber + typeof theNumber)
+
+// Declare uma variável com a sua idade
+var idade = 20
+console.log(idade + typeof idade)
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Ruan',
+    sobrenome = 'Lima',
+    nomeCompleto = `${nome} ${sobrenome} `
+console.log(nomeCompleto + typeof nomeCompleto)
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "'It's time' "
+console.log(frase + typeof frase)
+
+// Verifique o tipo da variável que contém o seu nome
+var meuNome = 'Ruan'
+console.log(typeof meuNome)
---
+++ js-iniciante/variaveis/script.js
@@ -0,0 +1,24 @@
+// Declarar uma variável com o seu nome
+var nome = 'Ruan'
+console.log(nome)
+
+// Declarar uma variável com a sua idade
+var idade = 20
+console.log(idade)
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza'
+console.log(comidaFavorita)
+
+// Declarar 5 variáveis diferentes sem valores
+var roupaPreferida,
+    time_do_coracao,
+    $livroPreferido,
+    $restaurante_Preferido,
+    $neymar10_
+
+time_do_coracao = 'Vasco'
+console.log(time_do_coracao)
