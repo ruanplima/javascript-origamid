ced59b6fbbb1c2c7e1cd5f38d74ac33d06bfb299
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2023-10-17 16:25:21 -0300

    Create Arrays-e-Loops

---
+++ js-iniciante/arrays-e-loops/script.js
@@ -0,0 +1,14 @@
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+var champions = ['1958', '1962', '1970', '1994', '2002']
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+for (var i = 0; i <= champions.length; i++) {
+    console.log(`O Brasil foi campeão em ${champions[i]}`)
+}
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
