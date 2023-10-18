3f9c8b014830c0ad461caffa77eab13bb3c3fbdb
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2023-10-18 15:33:37 -0300

    Create Ex DOM

---
+++ dom-iniciante/o-que-e/script.js
@@ -0,0 +1,15 @@
+// Retorne o url da página atual utilizando o objeto window
+let link = window.location.href
+console.log(link)
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+let vascao = document.querySelector('.ativo')
+
+// Retorne a linguagem do navegador
+let linguagem = window.navigator.language
+console.log(linguagem)
+
+// Retorne a largura da janela 
+const width = window.innerWidth
+console.log(width)
