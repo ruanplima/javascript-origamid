cce660591c9b979b6966313b7e7596455c4c3b9e
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2024-01-08 12:17:23 -0300

    Objetos - Native Host e User

---
+++ objetos/native-host-user/script.js
@@ -0,0 +1,12 @@
+// Liste 5 objetos nativos
+//OBJECT, STRING, ARRAY, FUNCTION, BOOLEAN
+
+
+// Liste 5 objetos do browser
+//NODELIST, HTMLCOLLECTION, ELEMENT, ALERT, NAVIGATOR
+
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+
