51163b09d6e1fe16aefa54910e1bb80d01b87631
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2024-05-01 11:10:13 -0300

    Update js

--- objetos/function/script.js
+++ objetos/function/script.js
@@ -6,11 +6,40 @@ console.log(areaQuadrado(5))
 console.log(areaQuadrado.length) // RETORNA O TOTAL DE PARÂMETROS DA FUNÇÃO
 console.log(areaQuadrado.name) // RETORNA O NOME DA FUNÇÃO
 
-// -//-
+
 
 function darTchau(nome, idade, aniversario){
     return 'Tchau ' + nome + ', sua idade é ' + idade + '? É verdade que você faz aniversário dia ' + aniversario + '?'
 }
 
 console.log(darTchau('Ruan', 20, '26 de maio'))
+
+
+
+function carro(modelo){
+    console.log(this)
+    console.log(`Comprei um ${this.carro} modelo ${modelo} do ano ${this.ano}`)
+}
+
+carro.call({carro: 'Fiat', ano: 2018}, 'Uno')
+
+
+
+const car = ["Fiat", "Gol", "Palio"]
+const frutas = ["Banana", "Laranja", "Manga"]
+console.log(car, frutas)
+
+car.forEach.call(frutas, (i) => {
+    console.log(i)
+})
+
+
+
+function Dom(seletor){
+    this.element = document.querySelector(seletor)
+}
+
+const ul = new Dom('ul')
+
+console.log(ul.element)
 
